e701704f989dacf3e18687c6f373c8be9ab9a48c
Tyler Falther <falther97@gmail.com>
2019-04-02 14:40:00 -0400

    Updated CalculateHash and LookForHash

--- SignatureDetection/Hash/CalculateHash.c
+++ SignatureDetection/Hash/CalculateHash.c
@@ -8,19 +8,23 @@
 ** in the the file of the users choice.					**
 ********************************************************/
 
-
+// Needed libraries
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+// Define the files that can be read
 #define MtoXEncrypted 1
 #define MtoXDecrypted 2
 
+// Paths for all hashes for each file to be read
 #define EncryptedFilePath "MtoXEncrypted.txt"
 #define DecryptedFilePath "MtoXDecrypted.txt"
 
+// Max file name length
 #define MAXLENGTH 20
 
+// to clear input from scanf
 void clearInput() {
 	int c;
 	while ((c = getchar()) != '\n' && c != EOF);
@@ -28,15 +32,14 @@ void clearInput() {
 }
 
 int main() {
-	// Get user input
-	// Read file
-		// Calculate hash as int?
-	// write value to end of file
 
+	// set up current choice and loop
 	int currentChoice = 0;
 	int cont = 1;
 
+	// run until user exits
 	while (cont) {
+		// Get type of file
 		printf("What file are you scanning? \n");
 		printf("1. MtoX Encrypted \n");
 		printf("2. MtoX Decrypted \n");
@@ -46,21 +49,25 @@ int main() {
 		clearInput();
 		printf("\n");
 
+		// break if a valid file isn't entered
 		if (currentChoice != MtoXEncrypted && currentChoice != MtoXDecrypted) {
 			cont = 0;
 			break;
 		}
 
+		// Get the file to be calculated
 		int valid = 0;
 		char inFileName[MAXLENGTH];
 		FILE *inFile;
 
+		// Run until a valid file is entered 
 		while (!valid) {
+			// get file name
 			printf("Please enter the file name: ");
 			fgets(inFileName, MAXLENGTH, stdin);
 			fflush(0);
 
-			// clean up input
+			// clean up file name
 			int i;
 			for(i = 0 ; i < MAXLENGTH ; i++)
      		{
@@ -69,9 +76,10 @@ int main() {
         		}
      		}
 
-
+     		// try to open file
 			inFile = fopen(inFileName, "r+");
 
+			// if file cannot be opend, ask the user to try again
 			if (inFile == NULL) {
 				printf("Invalid file, please try again. \n");
 			} else {
@@ -81,30 +89,35 @@ int main() {
 		}
 
 
+		// Get the hash file to appened to
 		FILE *outputFile;
-
 		if (currentChoice == MtoXEncrypted) {
 			outputFile = fopen(EncryptedFilePath, "a");
 
 		} else if (currentChoice == MtoXDecrypted) {
 			outputFile = fopen(DecryptedFilePath, "a");
 		} 
+		// if hash file cannot be opend, end program
 		if (outputFile == NULL) {
 			printf("Unable to open output file, ending program. \n");
 			return 1;
 		}
 
+		// calculate hash - just a sum of all characters ¯\_(ツ)_/¯
+		// can probably make this more sophisticated if needed
 		int value = 0;
 		int ch;
 		while ((ch = fgetc(inFile)) != EOF) {
 			value += ch;
 		}
 
+		// print the hash value to the output file
 		fprintf(outputFile, "%d \n", value);
 
 		printf("%s has been  read. \n ", inFileName);
 		printf("\n");
 
+		// close files 
 		fclose(outputFile);
 		fclose(inFile);
 
--- SignatureDetection/Hash/LookForHash.c
+++ SignatureDetection/Hash/LookForHash.c
@@ -2,13 +2,46 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define EncryptedFilePath "MtoXEncrypted.txt"
+#define DecryptedFilePath "MtoXDecrypted.txt"
+#define PATHS "Paths.txt"
+
+// Cleans up any junk that may have been added to the file. This might have to be
+// integrated in with just scanning it (stop the scan at a certain pont)
+void cleanJunk() {
+
+}
+
 int main() {
 
+	printf("Starting Program... \n");
 
-	// Get list of directories to check
+	FILE *paths;
+	paths = fopen(PATHS, "r");
+
+	if (paths == NULL) {
+		printf("Error reading Paths.txt. Ending program \n");
+		return 1;
+	}
+
+	int numPaths = 0;
+
+	int ch;
+	while((ch = fgetc(paths)) != EOF) {
+		if (ch == '\n') {
+			numPaths++;
+		}
+	}
+	print("%d", numPaths);
+
+	rewind(paths);
+
+	char *paths[numPaths];
+
+	// Get list of directories to check - from a file
 
 	// For each directory
-	
+
 		// Search for MtoX signature - Encrypted
 
 		// Search for MtoX signature - Decrypted
@@ -17,9 +50,4 @@ int main() {
 
 
 	return 0;
-}
-
-
-void cleanJunk() {
-
 }
