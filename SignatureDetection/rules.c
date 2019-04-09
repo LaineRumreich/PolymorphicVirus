62780474a91255fe183ad2168d4137c3e37d7291
pwalter3 <patwalter332@gmail.com>
2019-04-09 13:19:39 -0400

    My part is now done

--- SignatureDetection/rules.c
+++ SignatureDetection/rules.c
@@ -2,21 +2,54 @@
 #include <stdlib.h>
 #include <string.h>
 #include <windows.h>
+#include <dirent.h>
 
 void checkRules(char*);
 int match(char*, char*);
 int matchhere(char*, char*);
 int matchstar(int, char*, char*);
-void codeRed(char *);
-void codeYellow(char *);
+void codeRed(char[]);
+void codeYellow(char[]);
 
-const char PATH;
-char IGNORE_THESE_FILES[10];
+const char PATH[256] = "test";
+char IGNORE_THESE_FILES[10][256];
 int NUM_FILES_TO_IGNORE;
-int CURRENT_SIZE;
+int CURRENT_SIZE = 10;
 
 int main () {
-	checkRules("xichigan.c");
+	// run a continuous loop checking the files in the path for the virus
+	while(1) {
+		//open directory to check 
+		DIR *currDir;
+  		struct dirent *dir;
+  		currDir = opendir(PATH);
+  		if (currDir != NULL) {
+  			// read each file
+    		while ((dir = readdir(currDir)) != NULL) {
+				int check = 1;
+    			// calculate the hash for the current file
+    			char fullFilePath[100];
+    			strcpy(fullFilePath, PATH);
+    			strcat(fullFilePath, "/");
+    			strcat(fullFilePath, dir->d_name);
+      			//check to make sure that file is not to be ignored
+				for(int i = 0; i < CURRENT_SIZE; i++) {
+					if(strncmp(dir->d_name, IGNORE_THESE_FILES[i], 256) == 0) {
+						check = 0;
+					}
+				}
+				if(check) {
+					checkRules(dir->d_name);
+				}
+		    }
+    		closedir(currDir);
+  		} else {
+  			// if the directory cannot be opened, let the user know
+  			printf("Unable to open directory: %s \n \n", PATH);
+  		}
+		//sleep for 10 seconds 
+		Sleep(10000);
+	}
 }
 
 
@@ -24,7 +57,6 @@ void checkRules(char* name) {
 //check name for virus
 char *regex;
 int check = 0;
-
 //check for .doc, .txt, and .pdf extensions and ignore them 
 check = match(".*.doc", name);
 if(check) {
@@ -154,12 +186,14 @@ if(check) {
     }
 
 /*Alert the user that a file is a virus and delete that file*/
-void codeRed(char *virusName) {
+void codeRed(char virusName[]) {
 	char buf[512];
-	char* fullpath;
+	char fullpath[256];
 	int status;
 	//get the full path name
-	fullpath = PATH + virusName;
+	strcpy(fullpath, PATH);
+    strcat(fullpath, "/");
+    strcat(fullpath, virusName);
 	//first delete the file
 	status = remove(fullpath);
 	//check to see if the file was deleted 
@@ -174,9 +208,9 @@ void codeRed(char *virusName) {
 	}
 }
 
-void codeYellow(char* virusName) {
+void codeYellow(char virusName[]) {
 	char buf[512];
-	char* fullpath;
+	char fullpath[256];
 	int status;
 	int choice;
 	
@@ -188,23 +222,25 @@ void codeYellow(char* virusName) {
 		NUM_FILES_TO_IGNORE++;
 		//check to see if the array needs to be increased
 		if(NUM_FILES_TO_IGNORE > CURRENT_SIZE - 1) {
-			char temp[CURRENT_SIZE];
+			char temp[CURRENT_SIZE][256];
 			for(int i = 0; i < CURRENT_SIZE; i++) {
-				temp[i] = IGNORE_THESE_FILES[i];
+				strcpy(temp[i], IGNORE_THESE_FILES[i]);
 			}
-			char IGNORE_THESE_FILES[CURRENT_SIZE * 2];
+			char IGNORE_THESE_FILES[CURRENT_SIZE * 2][256];
 			//add filenames back to the array
 			for(int i = 0; i < CURRENT_SIZE; i++) {
-				IGNORE_THESE_FILES[i] = temp[i];
+				strcpy(IGNORE_THESE_FILES[i], temp[i]);
 			}
-			IGNORE_THESE_FILES[NUM_FILES_TO_IGNORE] = *virusName;
+			strcpy(IGNORE_THESE_FILES[NUM_FILES_TO_IGNORE], virusName);
 			CURRENT_SIZE *= 2;
 		} else {
-			IGNORE_THESE_FILES[NUM_FILES_TO_IGNORE] = *virusName;
+			strcpy(IGNORE_THESE_FILES[NUM_FILES_TO_IGNORE], virusName);
 		}
 	} else {
 		//get the full path name
-		fullpath = PATH + virusName;
+		strcpy(fullpath, PATH);
+		strcat(fullpath, "/");
+		strcat(fullpath, virusName);
 		//first delete the file
 		status = remove(fullpath);
 		//check to see if the file was deleted 
